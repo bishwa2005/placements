3195a8bb3abc57a7d37de2d73c8f465dcb302a25
Bishwa Ranjan Routray <br180905@gmail.com>
2026-03-10 19:11:15 +0530

    Time: 8 ms (52.09%), Space: 23.7 MB (37.04%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    void dfs(int r,int c,vector<vector<int>>& heights,
+             vector<vector<int>>& vis){
+
+        int m = heights.size();
+        int n = heights[0].size();
+
+        vis[r][c] = 1;
+
+        int drow[4] = {-1,0,1,0};
+        int dcol[4] = {0,1,0,-1};
+
+        for(int k=0;k<4;k++){
+
+            int nr = r + drow[k];
+            int nc = c + dcol[k];
+
+            if(nr>=0 && nc>=0 && nr<m && nc<n 
+               && !vis[nr][nc] 
+               && heights[nr][nc] >= heights[r][c]){
+
+                dfs(nr,nc,heights,vis);
+            }
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+
+        int m = heights.size();
+        int n = heights[0].size();
+
+        vector<vector<int>> pac(m,vector<int>(n,0));
+        vector<vector<int>> atl(m,vector<int>(n,0));
+
+        // Pacific (top row)
+        for(int j=0;j<n;j++)
+            dfs(0,j,heights,pac);
+
+        // Pacific (left column)
+        for(int i=0;i<m;i++)
+            dfs(i,0,heights,pac);
+
+        // Atlantic (bottom row)
+        for(int j=0;j<n;j++)
+            dfs(m-1,j,heights,atl);
+
+        // Atlantic (right column)
+        for(int i=0;i<m;i++)
+            dfs(i,n-1,heights,atl);
+
+        vector<vector<int>> ans;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(pac[i][j] && atl[i][j])
+                    ans.push_back({i,j});
+            }
+        }
+
+        return ans;
+    }
+};
