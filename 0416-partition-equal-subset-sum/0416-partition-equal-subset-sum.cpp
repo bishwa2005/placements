5c5ed769117fcc1259c1e135d992449876bc35a5
Bishwa Ranjan Routray <br180905@gmail.com>
2026-03-14 11:16:48 +0530

    Time: 135 ms (47.69%), Space: 17.5 MB (53.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    bool subarray(vector<int>& nums,int sum){
+    int subset(vector<int>& nums,int sum){
         int n=nums.size();
-
         vector<vector<bool>> dp(n+1,vector<bool>(sum+1));
 
         for(int i=0;i<=n;i++){
             dp[i][0]=true;
         }
-        for(int j=0;j<=sum;j++){
+
+        for(int j=1;j<=sum;j++){
             dp[0][j]=false;
         }
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=sum;j++){
-                
-                if(nums[i-1] <= j)
+                if(nums[i-1]<=j){
                     dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                
-                else
-                    dp[i][j] = dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
             }
         }
-        
+
         return dp[n][sum];
     }
 
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-
         int sum=0;
-        for(int i : nums){
-            sum+=i;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
 
         if(sum%2!=0) return false;
 
-        return subarray(nums,sum/2);
+        return subset(nums,sum/2);
     }
 };
