c262c447dfaccff1a0f0f9adc34144dfa88db890
Bishwa Ranjan Routray <br180905@gmail.com>
2026-03-14 11:08:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,26 +1,24 @@
 class Solution {
-public:
+  public:
     bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n=arr.size();
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1));
         
-        int n = arr.size();
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
         
-        vector<vector<bool>> dp(n+1, vector<bool>(sum+1,false));
-        
-        // base case
-        for(int i=0;i<=n;i++)
-            dp[i][0] = true;
-        
-        for(int j=1;j<=sum;j++)
-            dp[0][j] = false;
+        for(int j=1;j<=sum;j++){
+            dp[0][j]=false;
+        }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=sum;j++){
-                
-                if(arr[i-1] <= j)
+                if(arr[i-1]<=j){
                     dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-                
-                else
-                    dp[i][j] = dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
             }
         }
         
