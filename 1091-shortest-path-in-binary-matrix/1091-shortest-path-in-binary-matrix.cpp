b289cf4c421639c88db3b25bf850be338f9c291d
Bishwa Ranjan Routray <br180905@gmail.com>
2026-03-15 22:03:44 +0530

    Time: 19 ms (51.37%), Space: 26.3 MB (33.68%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
         int n = grid.size();
-        
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+
+        if(grid[0][0] == 1) return -1;
 
         queue<pair<pair<int,int>,int>> q;
         vector<vector<int>> vis(n, vector<int>(n,0));
@@ -12,29 +11,24 @@ public:
         q.push({{0,0},1});
         vis[0][0] = 1;
 
-        int drow[8] = {-1,-1,-1,0,0,1,1,1};
-        int dcol[8] = {-1,0,1,-1,1,-1,0,1};
+        int drow[] = {-1,-1,-1,0,0,1,1,1};
+        int dcol[] = {-1,0,1,-1,1,-1,0,1};
 
         while(!q.empty()){
-
             int x = q.front().first.first;
             int y = q.front().first.second;
-            int dist = q.front().second;
+            int d = q.front().second;
             q.pop();
 
-            if(x == n-1 && y == n-1)
-                return dist;
+            if(x == n-1 && y == n-1) return d;
 
             for(int i=0;i<8;i++){
-
                 int nx = x + drow[i];
                 int ny = y + dcol[i];
 
-                if(nx>=0 && ny>=0 && nx<n && ny<n 
-                && grid[nx][ny]==0 && vis[nx][ny]==0){
-
+                if(nx>=0 && ny>=0 && nx<n && ny<n && !vis[nx][ny] && grid[nx][ny]==0){
                     vis[nx][ny] = 1;
-                    q.push({{nx,ny},dist+1});
+                    q.push({{nx,ny}, d+1});
                 }
             }
         }
