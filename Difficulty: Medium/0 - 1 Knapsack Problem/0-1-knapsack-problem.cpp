ab0f7c40ff8698ce197faa336b1987c7ecec57f9
Bishwa Ranjan Routray <br180905@gmail.com>
2026-03-24 09:56:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int solve(int n,int W,vector<int>& val,vector<int>& wt,vector<vector<int>>& dp){
-        if(n==0 || W==0) return 0;
+    int find(int capacity, vector<int> &val, vector<int> &wt, int n, vector<vector<int>> &dp) {
+        if (n == 0 || capacity == 0) return 0;
 
-        if(dp[n][W] != -1) return dp[n][W];
+        if (dp[n][capacity] != -1) return dp[n][capacity];
 
-        if(wt[n-1] <= W){
-            return dp[n][W] = max(val[n-1] + solve(n-1,W-wt[n-1],val,wt,dp),
-                                  solve(n-1,W,val,wt,dp));
+        if (wt[n - 1] <= capacity) {
+            return dp[n][capacity] = max(
+                val[n - 1] + find(capacity - wt[n - 1], val, wt, n - 1, dp), // take
+                find(capacity, val, wt, n - 1, dp)                           // not take
+            );
+        } else {
+            return dp[n][capacity] = find(capacity, val, wt, n - 1, dp);
         }
-
-        return dp[n][W] = solve(n-1,W,val,wt,dp);
     }
 
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         int n = val.size();
-        vector<vector<int>> dp(n+1, vector<int>(W+1,-1));
-
-        return solve(n,W,val,wt,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+        return find(W, val, wt, n, dp);
     }
 };
