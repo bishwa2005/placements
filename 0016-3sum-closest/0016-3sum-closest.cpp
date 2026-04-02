f8c2c2f1ccfb9c16681611959ad59904364cf7ac
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-02 21:24:13 +0530

    Time: 11 ms (60.91%), Space: 14.1 MB (33.12%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int ans = nums[0] + nums[1] + nums[2];  // Initialize with the first possible triplet
+        int result = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < n - 2; i++) {
-            int s = i + 1;
-            int e = n - 1;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1, right = nums.size() - 1;
 
-            while (s < e) {
-                int current_sum = nums[i] + nums[s] + nums[e];
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
-                if (abs(current_sum - target) < abs(ans - target)) {
-                    ans = current_sum;
-                }
+                if (abs(target - sum) < abs(target - result))
+                    result = sum;
 
-                if (current_sum < target) {
-                    ++s;
-                } else if (current_sum > target) {
-                    --e;
-                } else {
-                    // Exact match
-                    return current_sum;
-                }
+                if (sum == target) return target;
+                else if (sum < target) left++;
+                else right--;
             }
         }
-        return ans;
+
+        return result;
     }
 };
