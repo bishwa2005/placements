56756ff9d6bec038676c77249158f16590802aaa
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-04 23:48:59 +0530

    Time: 433 ms (27.91%), Space: 317.5 MB (31.87%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
-        
-        for (auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        vector<vector<int>> adj(n);
+
+        for(auto it : edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        
-        queue<int> queue;
-        unordered_set<int> visited;
-        
-        queue.push(source);
-        visited.insert(source);
-        
-        while (!queue.empty()) {
-            int node = queue.front();
-            queue.pop();
-            if (node == destination) {
-                return true;
-            }
-            for (int neighbor : graph[node]) {
-                if (visited.find(neighbor) == visited.end()) {
-                    visited.insert(neighbor);
-                    queue.push(neighbor);
-                }
+
+        queue<int> que;
+        que.push(source);
+        vector<int> vis(n,0);
+        vis[source]=1;
+
+        while(que.size()){
+            int node=que.front();
+            que.pop();
+
+            for(auto i : adj[node]){
+                if(!vis[i])
+                    que.push(i);
+                    vis[i]=1;
             }
         }
-        
-        return false;
+
+        return vis[destination];
     }
 };
