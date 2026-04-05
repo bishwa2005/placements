0ee68ace38de6889fe4e27c2d81f987571e36d5b
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-05 19:59:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        int n = nums.size();
-        int sum = 0;
-        
-        for(int x : nums)
-            sum += x;
+        int n=nums.size();
 
-        if(sum < abs(target)) return 0;
-        
-        if((sum - target) % 2 != 0)
-            return 0;
+        int sum=0;
+        for(int i : nums){
+            sum+=i;
+        }
 
-        int diff = (sum - target) / 2;
+        int s=(sum+target)/2;
+        vector<vector<int>> dp(n+1,vector<int>(s+1,0));
 
-        vector<vector<int>> dp(n+1, vector<int>(diff+1,0));
-
-        for(int i=0;i<=n;i++)
-            dp[i][0] = 1;
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
 
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=diff;j++){
-                
-                if(nums[i-1] <= j)
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
-                else
+            for(int j=1;j<=s;j++){
+                if(nums[i-1]<=j)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                else 
                     dp[i][j] = dp[i-1][j];
             }
         }
 
-        return dp[n][diff];
+        return dp[n][s];
+        
     }
 };
