b3b8e9166fc05e716a6ddb1353d20ec6f2d8c3a7
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-05 20:01:13 +0530

    Time: 3 ms (85.29%), Space: 18.3 MB (34.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n = nums.size();
 
-        int sum=0;
-        for(int i : nums){
-            sum+=i;
-        }
+        int sum = 0;
+        for (int x : nums) sum += x;
 
-        int s=(sum+target)/2;
-        vector<vector<int>> dp(n+1,vector<int>(s+1,0));
+        // 🔴 Edge case check
+        if ((sum + target) % 2 != 0 || abs(target) > sum) return 0;
 
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
+        int s = (sum + target) / 2;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=s;j++){
-                if(nums[i-1]<=j)
-                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
-                else 
-                    dp[i][j] = dp[i-1][j];
+        vector<vector<long long>> dp(n + 1, vector<long long>(s + 1, 0));
+
+        // base case
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        // DP
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= s; j++) {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
             }
         }
 
         return dp[n][s];
-        
     }
 };
