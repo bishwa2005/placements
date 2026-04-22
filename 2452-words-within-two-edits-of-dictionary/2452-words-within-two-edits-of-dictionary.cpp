ca4088147f3693f247c7c10cce1172fb9bd1e855
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-22 09:06:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (97.33%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    int check(string &w1,string &w2){
-        int n = w1.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(w1[i]!=w2[i]) cnt++;
+    int check(string &w1, string &w2){
+        int cnt = 0;
+        for(int i = 0; i < w1.size(); i++){
+            if(w1[i] != w2[i]){
+                cnt++;
+                if(cnt > 2) return cnt; // early break (optimization)
+            }
         }
         return cnt;
     }
 
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        int n = queries.size();
-        int m = dictionary.size();
-
-        int i=0,j=0;
-
         vector<string> ans;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                string w1 = queries[i];
-                string w2 = dictionary[j];
-
-                if(check(w1,w2)>2) continue;
-                else{
-                    ans.push_back(w1);
+        for(string &q : queries){
+            for(string &d : dictionary){
+                if(check(q, d) <= 2){
+                    ans.push_back(q);
                     break;
                 }
             }
-        }
-
-        while(i<n && j<m){
-            
         }
 
         return ans;
