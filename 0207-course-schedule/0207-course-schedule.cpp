2354cab1f85a555ff5dad8bf29bdce42684f4988
Bishwa Ranjan Routray <br180905@gmail.com>
2026-04-26 13:12:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (80.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,29 +3,31 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         vector<int> indegree(numCourses);
-
-        for(auto it : prerequisites){
+        
+        for(auto &it : prerequisites){
             adj[it[1]].push_back(it[0]);
             indegree[it[0]]++;
         }
 
         queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0) q.push(i);
-        }
-
-        vector<int> topo;
-
-        while(q.size()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
 
-        return (topo.size()==numCourses);
+        vector<int> ans;
+        while(q.size()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+
+        }
+
+        return ans.size()==numCourses;
     }
 };
