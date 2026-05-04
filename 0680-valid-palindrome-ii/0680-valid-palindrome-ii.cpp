7ebbb9fb2db77c969a656be745627f93510d5fd3
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-04 20:48:28 +0530

    Time: 4 ms (57.96%), Space: 22 MB (78.81%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     bool check(int i,int j,string &s){
         while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+            if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
+
     bool validPalindrome(string s) {
         int i=0;
         int j=s.size()-1;
 
         while(i<j){
-            if(s[i]!=s[j]){
-                return check(i,j-1,s) || check(i+1,j,s);
-            }
+            if(s[i]!=s[j]) return check(i,j-1,s) || check(i+1,j,s);
             i++;
             j--;
-        }
+        }    
 
         return true;
     }
