fa6c63af0b29f456652bee86aab151ca50b926b1
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-12 09:30:03 +0530

    Time: 57 ms (41.75%), Space: 97.3 MB (85.92%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    bool ispossible(vector<vector<int>>& tasks,int mid){
+        for(auto &it : tasks){
+            int actual=it[0];
+            int mini=it[1];
+
+            if(mid<mini) return false;
+
+            mid-=actual;
+        }
+        return true;
+    }
+
+
     static bool cmp(vector<int>& a, vector<int>& b) {
         return (a[1] - a[0]) > (b[1] - b[0]);
     }
 
     int minimumEffort(vector<vector<int>>& tasks) {
-        sort(tasks.begin(), tasks.end(), cmp);
 
-        int spent = 0;
-        int ans = 0;
+        sort(tasks.begin(),tasks.end(),cmp);
 
-        for (auto &task : tasks) {
-            int actual = task[0];
-            int minimum = task[1];
+        int l=0;
+        int r=1e9;
+        int result;
+        while(l<=r){
+            int mid = l+(r-l)/2;
 
-            ans = max(ans, spent + minimum);
-            spent += actual;
+            if(ispossible(tasks,mid)){
+                r=mid-1;
+                result=mid;
+            }
+            else{
+                l=mid+1;
+            }
         }
 
-        return ans;
+        return result;
     }
 };
