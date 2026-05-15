0fd304a971bfa55df9af2f5631a12de14544ccc5
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-15 14:51:47 +0530

    Time: 8 ms (83.47%), Space: 12 MB (21.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,18 +11,15 @@ public:
     bool hasCycle(ListNode *head) {
         if(!head || !head->next) return false;
 
-        ListNode *fast = head;
-        ListNode *slow = head;
+        ListNode* slow=head;
+        ListNode* fast=head->next;
 
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
+        while(slow!=fast){
+            if(!fast || !fast->next) return false;
             slow=slow->next;
-
-            if(fast==slow){
-                return true;
-            }
+            fast=fast->next->next;
         }
 
-        return false;
+        return true;
     }
 };
