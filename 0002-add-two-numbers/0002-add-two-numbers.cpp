69b63f7ac96ebd7dc24915b8ac4c9431208126fb
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-15 23:02:31 +0530

    Time: 0 ms (100%), Space: 77.2 MB (11.64%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,31 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
         int carry = 0;
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
+        while(l1 || l2 || carry){
 
-            int sum = digit1 + digit2 + carry;
+            int a = (l1) ? l1->val : 0;
+            int b = (l2) ? l2->val : 0;
+
+            int sum = a + b + carry;
+
             int digit = sum % 10;
             carry = sum / 10;
 
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
+            ListNode* newnode = new ListNode(digit);
+
+            tail->next = newnode;
             tail = tail->next;
 
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+            l1 = (l1) ? l1->next : NULL;
+            l2 = (l2) ? l2->next : NULL;
         }
 
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        return dummy.next;
     }
 };
