07ff982d1f546d5b982ca41ad173ae503191e847
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-21 11:39:18 +0530

    Time: 0 ms (100%), Space: 23.5 MB (93.15%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,25 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    int diameter(TreeNode* root, int& result) {
-        if(!root)
-            return 0;
+    int solve(TreeNode* root,int& ans){
+        if(root==NULL) return 0;
         
-        int left  = diameter(root->left, result);
-        int right = diameter(root->right, result);
-        
-        result = max(result, left + right); //What if I longest diamenter passes through left -> me -> right
-        
-        return max(left, right) + 1; //But I will return max I can afford if (left -> me -> right) was not answer
-        
+        int left = solve(root->left,ans);
+        int right = solve(root->right,ans);
+
+        ans=max(left+right,ans);
+        return max(left,right)+1;
     }
-    
     int diameterOfBinaryTree(TreeNode* root) {
-        
-        int result = INT_MIN;
-        
-        diameter(root, result);
-        
-        return result;
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
     }
 };
