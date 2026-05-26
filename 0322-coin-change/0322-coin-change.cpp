538c7f2dd71d99c6063bbaf844610e1e17592053
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-26 12:10:27 +0530

    Time: 43 ms (37.5%), Space: 53.1 MB (11.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -7,21 +7,19 @@ public:
         for(int i=0;i<=n;i++){
             dp[i][0]=0;
         }
-        for(int j=0;j<=amount;j++){
+        for(int j=1;j<=amount;j++){
             dp[0][j]=INT_MAX-1;
         }
         for(int j=1;j<=amount;j++){
-            if(j%coins[0]==0)           
-                dp[1][j]=j/coins[0];
-            else 
-                dp[1][j]=INT_MAX-1;    
+            if(j%coins[0]==0) dp[1][j]=j/coins[0];
+            else dp[1][j]=INT_MAX-1;
         }
 
         for(int i=2;i<=n;i++){
             for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j)
-                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
-                else 
+                if(coins[i-1]<=j) 
+                    dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                else
                     dp[i][j]=dp[i-1][j];
             }
         }
