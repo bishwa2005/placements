2cdbddc256b98d4c248d0238c745d2af1e44a097
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-26 11:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -4,20 +4,22 @@ class Solution {
   public:
     int cutRod(vector<int> &price) {
         // code here
-        
         int n=price.size();
-        vector<int> length;
-        for(int i=1;i<=n;i++)
-            length.push_back(i);
+        vector<int> len;
+        for(int i=1;i<=n;i++){
+            len.push_back(i);
+        }        
         
         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(length[i-1]<=j)
-                    dp[i][j]=max(price[i-1]+dp[i][j-length[i-1]],dp[i-1][j]);
-                else
+                if(len[i-1]<=j){
+                    dp[i][j]=max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
+                }
+                else{
                     dp[i][j]=dp[i-1][j];
+                }
             }
         }
         
