d485eb2dfc9af6bbe1ef003960cd6d60fbd2e2e7
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-28 08:44:27 +0530

    Time: 7 ms (24.44%), Space: 14.8 MB (35.72%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        string word = "";
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] != ' ') {
+                word += s[i];
+            }
+            else {
+                reverse(word.begin(), word.end());
+                ans += word;
+                ans += ' ';
+                word = "";
+            }
+        }
+
+        // last word
+        reverse(word.begin(), word.end());
+        ans += word;
+
+        return ans;
+    }
+};
