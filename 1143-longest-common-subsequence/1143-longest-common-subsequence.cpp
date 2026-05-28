806163ee877336dbd5bf7374802e171061e562bf
Bishwa Ranjan Routray <br180905@gmail.com>
2026-05-28 11:06:31 +0530

    Time: 28 ms (39.49%), Space: 27.4 MB (67.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,9 +11,8 @@ public:
                 if(text1[i-1]==text2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
-                else{
+                else
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
             }
         }
 
