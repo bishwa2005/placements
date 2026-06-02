9c34e8a7b5dcdf750bddb921d73c5102c6f5113a
Bishwa Ranjan Routray <br180905@gmail.com>
2026-06-02 12:37:14 +0530

    Time: 30 ms (33.66%), Space: 24.5 MB (10.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>> &vis,vector<vector<char>> &grid){
-        int n = grid.size();
-        int m = grid[0].size();
+    void solve(int i, int j, vector<vector<char>>& grid,
+               vector<vector<int>>& vis) {
 
-        vis[i][j] = 1;
+        queue<pair<int,int>> q;
+        q.push({i,j});
 
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,-1,0,1};
+        int dx[] = {-1,0,1,0};
+        int dy[] = {0,-1,0,1};
 
-        for(int k=0;k<4;k++){
-            int nx = i + drow[k];
-            int ny = j + dcol[k];
+        while(!q.empty()) {
+            auto [x,y] = q.front();
+            q.pop();
 
-            if(nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny] && grid[nx][ny]=='1'){
-                dfs(nx,ny,vis,grid);
+            for(int k=0;k<4;k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx >= 0 && nx < grid.size() &&
+                   ny >= 0 && ny < grid[0].size() &&
+                   grid[nx][ny] == '1' &&
+                   !vis[nx][ny]) {
+
+                    vis[nx][ny] = 1;
+                    q.push({nx,ny});
+                }
             }
         }
     }
@@ -23,14 +33,16 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n, vector<int>(m,0));
+
         int cnt = 0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(!vis[i][j] && grid[i][j] == '1') {
+                    vis[i][j] = 1;
+                    solve(i,j,grid,vis);
                     cnt++;
-                    dfs(i,j,vis,grid);
                 }
             }
         }
