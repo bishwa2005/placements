086e31ade52035a848e13a2a710760b7fab24a36
Bishwa Ranjan Routray <br180905@gmail.com>
2026-06-05 11:40:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
@@ -6,31 +6,33 @@ class Solution {
         for(auto it : edges){
             int u=it[0];
             int v=it[1];
+            
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         
-        vector<int> dist(V,-1);
         vector<int> vis(V,0);
-        queue<pair<int,int>> q;
         
+        queue<pair<int,int>> q;
         q.push({src,0});
         vis[src]=1;
         
+        vector<int> ans(V,-1);
+        
         while(q.size()){
-            int node=q.front().first;
-            int d=q.front().second;
+            auto [node,d] = q.front();
             q.pop();
-            dist[node]=d;
+            
+            ans[node]=d;
             
             for(auto it : adj[node]){
                 if(!vis[it]){
-                    vis[it]=1;
                     q.push({it,d+1});
+                    vis[it]=1;
                 }
             }
         }
         
-        return dist;
+        return ans;
     }
 };
