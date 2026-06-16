b189deae9bb055969a87d00f5081763f43bf16dd
Bishwa Ranjan Routray <br180905@gmail.com>
2026-06-16 10:47:35 +0530

    Time: 0 ms (100%), Space: 19.7 MB (43.24%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     typedef unsigned long long ll;
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)   
+        if (!root)
             return 0;
         queue<pair<TreeNode*, ll>> que;
         que.push({root, 0});
         ll maxWidth = 0;
-        
-        while(!que.empty()) {
+
+        while (!que.empty()) {
             int n = que.size();
             ll f = que.front().second;
             ll l = que.back().second;
-            maxWidth = max(maxWidth, l-f+1);
-            
-            while(n--) {
+            maxWidth = max(maxWidth, l - f + 1);
+
+            while (n--) {
                 TreeNode* curr = que.front().first;
-                ll d          = que.front().second;
+                ll d = que.front().second;
                 que.pop();
-                if(curr->left) {
-                    que.push({curr->left, 2*d+1});
+                if (curr->left) {
+                    que.push({curr->left, 2 * d + 1});
                 }
-                if(curr->right) {
-                    que.push({curr->right, 2*d+2});
+                if (curr->right) {
+                    que.push({curr->right, 2 * d + 2});
                 }
             }
         }
