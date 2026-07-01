3f24da1a7ae5340f98ce75f75017435300af1b65
Bishwa Ranjan Routray <br180905@gmail.com>
2026-07-01 12:08:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -8,10 +8,10 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=capacity;j++){
                 if(wt[i-1]<=j){
-                    dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+                    dp[i][j] = max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
