de9ef1eb17df0a058e2a1a4ee78df52daff6b48f
Bishwa Ranjan Routray <br180905@gmail.com>
2026-07-01 12:28:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -1,5 +1,3 @@
-// User function Template for C++
-
 class Solution {
   public:
     int cutRod(vector<int> &price) {
@@ -8,18 +6,16 @@ class Solution {
         vector<int> len;
         for(int i=1;i<=n;i++){
             len.push_back(i);
-        }        
+        }
         
         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(len[i-1]<=j){
-                    dp[i][j]=max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+                if(len[i-1]<=j)
+                    dp[i][j] = max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
+                else
+                    dp[i][j] = dp[i-1][j];
             }
         }
         
