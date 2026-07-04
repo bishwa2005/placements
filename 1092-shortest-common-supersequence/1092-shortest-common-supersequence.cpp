e8aa52f696b7b84bfcbcb3fd737f1fbe903bb41f
Bishwa Ranjan Routray <br180905@gmail.com>
2026-07-04 10:29:14 +0530

    Time: 27 ms (36.6%), Space: 26.9 MB (42.79%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,53 +1,55 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
+        int m = str1.size();
+        int n=str2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
 
-        // Step 1: Build LCS table
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(str1[i-1] == str2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
 
-        // Step 2: Build SCS using dp table
-        int i = n, j = m;
-        string ans = "";
+        int k=m;
+        int l=n;
 
-        while(i > 0 && j > 0){
-            if(str1[i-1] == str2[j-1]){
-                ans += str1[i-1];
-                i--; j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                ans += str1[i-1];
-                i--;
+        string ans="";
+
+        while(k>0 && l>0){
+            if(str1[k-1]==str2[l-1]){
+                ans+=str1[k-1];
+                k--;
+                l--;
             }
             else{
-                ans += str2[j-1];
-                j--;
+                if(dp[k-1][l]>dp[k][l-1]){
+                    ans+=str1[k-1];
+                    k--;
+                }
+                else{
+                    ans+=str2[l-1];
+                    l--;
+                }
             }
         }
 
-        // Step 3: Add remaining characters
-        while(i > 0){
-            ans += str1[i-1];
-            i--;
+        while(k>0){
+            ans+=str1[k-1];
+            k--;
+        }
+        while(l>0){
+            ans+=str2[l-1];
+            l--;
         }
 
-        while(j > 0){
-            ans += str2[j-1];
-            j--;
-        }
-
-        // Step 4: Reverse answer
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
 
         return ans;
     }
