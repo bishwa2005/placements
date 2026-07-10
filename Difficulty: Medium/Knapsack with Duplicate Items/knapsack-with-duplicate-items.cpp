c3e546d69aadf4b2188102a2bcccd93fc3b261dd
Bishwa Ranjan Routray <br180905@gmail.com>
2026-07-10 12:37:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -3,16 +3,16 @@ class Solution {
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
         int n=val.size();
+        
         vector<vector<int>> dp(n+1,vector<int>(capacity+1,0));
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=capacity;j++){
-                if(wt[i-1]<=j){
-                    dp[i][j] = max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+                if(wt[i-1] <= j){
+                    dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
                 }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+                else 
+                    dp[i][j]=dp[i-1][j];
             }
         }
         
