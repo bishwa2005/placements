745ca44cc768c41bc9a5dbbe0ab16bd3ebeda25b
Bishwa Ranjan Routray <br180905@gmail.com>
2026-07-31 08:29:47 +0530

    Time: 31 ms (27.82%), Space: 24.2 MB (21.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void solve(int i, int j, vector<vector<char>>& grid,
-               vector<vector<int>>& vis) {
-
+    void bfs(int r,int c,vector<vector<int>> &vis,vector<vector<char>> &grid){
+        int m=grid.size();
+        int n=grid[0].size();
         queue<pair<int,int>> q;
-        q.push({i,j});
+        q.push({r,c});
 
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,-1,0,1};
-
-        while(!q.empty()) {
+        while(q.size()){
             auto [x,y] = q.front();
             q.pop();
 
-            for(int k=0;k<4;k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
+            int dx[]={-1,0,1,0};
+            int dy[]={0,-1,0,1};
 
-                if(nx >= 0 && nx < grid.size() &&
-                   ny >= 0 && ny < grid[0].size() &&
-                   grid[nx][ny] == '1' &&
-                   !vis[nx][ny]) {
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
 
-                    vis[nx][ny] = 1;
+                if(nx<m && ny<n && nx>=0 && ny>=0 && !vis[nx][ny] && grid[nx][ny]=='1'){
+                    vis[nx][ny]=1;
                     q.push({nx,ny});
                 }
             }
@@ -30,18 +26,16 @@ public:
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,0));
 
-        vector<vector<int>> vis(n, vector<int>(m,0));
+        int cnt=0;
 
-        int cnt = 0;
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(!vis[i][j] && grid[i][j] == '1') {
-                    vis[i][j] = 1;
-                    solve(i,j,grid,vis);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
+                    bfs(i,j,vis,grid);
                     cnt++;
                 }
             }
