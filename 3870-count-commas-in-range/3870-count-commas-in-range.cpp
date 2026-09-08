366c9d3b8d3134a3877d07cae6f2dca9c64c569a
Bishwa Ranjan Routray <br180905@gmail.com>
2026-09-08 11:58:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        long long ans = 0;
+        long long base = 1000;
+
+        while (base <= n) {
+            ans += (n - base + 1);
+            base *= 1000;
+        }
+
+        return ans;
+    }
+};
