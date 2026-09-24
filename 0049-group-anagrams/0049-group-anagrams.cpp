ed7bd7fb7e57ab2ee3e8648436fe695ec5a1940f
Bishwa Ranjan Routray <br180905@gmail.com>
2026-09-24 20:03:57 +0530

    Time: 16 ms (59.32%), Space: 25.1 MB (64.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,16 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mp;
 
-        for(auto st : strs){
-            string it = st;
-            sort(it.begin(),it.end());
-            mp[it].push_back(st);
+        for(auto it : strs){
+            string s=it;
+            sort(s.begin(),s.end());
+            mp[s].push_back(it);
         }
 
         vector<vector<string>> ans;
-
-        for(auto vec : mp){
-            ans.push_back(vec.second);
+        for(auto &[key,val] : mp){
+            ans.push_back(val);
         }
 
         return ans;
